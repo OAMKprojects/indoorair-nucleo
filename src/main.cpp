474eb91ad97aa3f65d6339b9ecc5eb0b05cf8b12
Yarviz <juzzzzi@hotmail.com>
2021-03-22 20:32:12 +0200

    analog sensor code started

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "mbed.h"
 #include "DHT22.h"
 #include "HCSR04.h"
+#include "TMP36GZ.h"
 
 int main()
 {
     Sensor      temp_sensor(D8);
     Ultrasonic  dist_sensor(D11, D15);
+    TempSensor  analog_temp(A0);
 
     DHT_info data;
     int error;
@@ -20,9 +22,10 @@ int main()
 
         if (error == NO_ERROR || error == ERR_PARITY_BIT)
         {
-            printf("Temperature : %i.%iC Humidity : %i.%i%% Distance: %i.%i cm\n", data.temperature / 10, data.temperature % 10, 
+            printf("Temperature : %i.%iC Humidity : %i.%i%% Distance: %i.%i cm Analog Temperature: %i V\n", data.temperature / 10, data.temperature % 10, 
                                                                                 data.humidity / 10, data.humidity % 10, 
-                                                                                distance / 10, distance % 10);
+                                                                                distance / 10, distance % 10,
+                                                                                analog_temp.read());
         }
         else {
             printf("Error nro: %i \n", error);
