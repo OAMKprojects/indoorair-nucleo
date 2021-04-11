610795d1a74ff1c813e6acfb25f7eac94693c3d2
Yarviz <juzzzzi@hotmail.com>
2021-04-11 16:13:36 +0300

    fysiikan osuuden koodi

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,43 @@
 #include "mbed.h"
-#include "DHT22.h"
+
+#define RESISTOR_OHM  10000
+#define READ_TIMES    10
+
+#define C1            1.340933450906873e-3
+#define C2            2.020648784092252e-4
+#define C3            1.958312651e-7
+
+float R1 = RESISTOR_OHM;
+float R2;
+float raw;
+
+float getCelcius(float raw_value)
+{
+    R2 = raw / (1.0 - raw) * R1;
+    double logR = log(R2);
+    double temp = 1.0 / (C1 + C2 * logR + C3 * pow(logR, 3)) - 273.15;
+    return temp;
+}
 
 int main()
 {
-    Sensor      temp_sensor(D8);
-    DHT_info data;
-    int error;
-
-    wait_us(100000);
+    AnalogIn dht(A0);
+    int i;
+    double temp;
 
     while (true) 
     {
-        error = temp_sensor.readValues(data);
-
-        if (error == NO_ERROR || error == ERR_PARITY_BIT)
-        {
-            printf("{temperature: %i.%i, humidity: %i.%i}\n", 
-                    data.temperature / 10, data.temperature % 10, 
-                    data.humidity / 10, data.humidity % 10);
+        raw = 0;
+        for (i = 0; i < READ_TIMES; i++) {
+            raw += dht.read();
+            wait_us(1000);
         }
-        else {
-            printf("{error: %i}\n", error);
-        }
+        
+        raw /= READ_TIMES;
+        temp = getCelcius(raw);
 
-        wait_us(1000000);
+        printf("Resistance: %i ohms, Temperature: %i.%i C\n", (int)R2, (int)temp, (int)(temp * 10) % 10);
+
+        ThisThread::sleep_for(1s);
     }
 }
-
